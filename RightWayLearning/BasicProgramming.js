56e1e598504a2fddde91e88710ec80cf93b57972
sachin.lagad <sachin.lagad@gmail.com>
2022-09-14 06:44:25 +0530

    first program

---
+++ RightWayLearning/BasicProgramming.js
@@ -0,0 +1,13 @@
+
+  let myName = "sachin";
+
+  let age = 32;
+
+  let bG = "AB+";
+
+  let height = null;
+
+  console.log(myName);
+  console.log(age);
+  console.log(bG);
+  console.log(height);
