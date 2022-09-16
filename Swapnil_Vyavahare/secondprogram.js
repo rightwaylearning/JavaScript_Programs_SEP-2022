29004502353a756dddf2e5331c67c63d962deefb
abc <swapnilplan@gmail.com>
2022-09-16 12:50:41 +0530

    substraction add

--- Swapnil_Vyavahare/secondprogram.js
+++ Swapnil_Vyavahare/secondprogram.js
@@ -1,7 +1,13 @@
 function addition(a,b){
 let sum= a+b;
-console.addition(sum);
+console.log(sum);
+}
+
+function substraction(a,b){
+    let sub = a-b;
+    console.log(sub);
 }
 
 addition(10,15);
 addition(5,25);
+substraction(10,5);
