69e385026e1b9285868c0d98a71aa5be9fbc3378
unknown <ashwinidewade23@gmail.com>
2022-09-18 23:29:40 +0530

    basic calculator

---
+++ Ashwini_Dewade/calculator.js
@@ -0,0 +1,28 @@
+
+function sum(a,b){
+    let s=a+b;
+    console.log(s);
+
+}
+function sub(a,b)
+{
+    let su=a-b;
+    console.log(su);
+
+}
+function mult(a,b)
+{
+    let m=a*b;
+    console.log(m);
+}
+function div(a,b)
+{
+    let d=a/b;
+    console.log(d);
+
+
+}
+sum(2,5);
+sub(5,1);
+mult(6,3);
+div(8,2);
