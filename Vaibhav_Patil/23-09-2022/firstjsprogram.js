d373cec40006be1a8c56d386825e60b9e61900c2
vaibhav Patil <vaibhavpatil8743@gmail.com>
2022-09-26 06:41:56 +0530

    hello

---
+++ Vaibhav_Patil/23-09-2022/firstjsprogram.js
@@ -0,0 +1,17 @@
+let num=10;
+console.log(typeof num);
+
+let str="vainbhav";
+console.log(typeof of);
+
+let flag=true;
+console.log(typeof str);
+
+let msg=null;
+console.log(typeof msg);
+
+let n=undefined;
+console.log(typeof n);
+
+
+
