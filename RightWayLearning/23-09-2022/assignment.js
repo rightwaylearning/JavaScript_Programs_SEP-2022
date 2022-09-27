af0e3109d99c3f517fcd9a69195a5a7c66a0e36e
sachin lagad <sachin.lagad@gmail.com>
2022-09-27 07:26:11 +0530

    array object related problems took

--- RightWayLearning/23-09-2022/assignment.js
+++ RightWayLearning/23-09-2022/assignment.js
@@ -1,10 +1,41 @@
 
-/// create one array of Employee object
-// Employee object propetries 
-/*
-name:
-salary:
-age:
+let employeeList = [
+    {
+        name:"ab",
+        age:34,
+        salary:56000
+    },
+    {
+        name:"KL",
+        age:54,
+        salary:156000
+    },
+    {
+        name:"HK",
+        age:44,
+        salary:156000
+    },
+    {
+        name:"kk",
+        age:31,
+        salary:16000
+    }
+];
 
-*/
-// 40+ >> employee_cat : "S"  or "J"
+let i = 0;
+while(i < employeeList.length){
+    if(employeeList[i].age > 40){
+        employeeList[i].bonus = 10000;
+    }
+    i = i +1;
+}
+console.log(employeeList);
+
+
+
+
+
+
+
+
+
