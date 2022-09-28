56ae5a31ab0cb0aa3a283a512c25807d538a43da
sachin lagad <sachin.lagad@gmail.com>
2022-09-28 07:18:01 +0530

    function

---
+++ RightWayLearning/28-09-2022/function_with_return_type.js
@@ -0,0 +1,51 @@
+
+//==============================
+// in javascript variable are typeless, value have type , according to data/
+// value assigned to variable we are going to decide data type of that variable
+
+let x;
+console.log(typeof x);
+x = 23;
+console.log(typeof x);
+x ={};
+console.log(typeof x);
+x = [];
+console.log(typeof x);
+x = function(){}
+console.log(typeof x);
+x =true;
+console.log(typeof x);
+x = null;
+console.log(typeof x);
+x ="abc";
+console.log(typeof x);
+x ='lmn';
+console.log(typeof x);
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+function getData(){
+   let f = function(){
+     console.log("Hello");
+   }
+   return f;
+}
+
+let data = getData();
+data(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
