0f9a478a381159fc17ae2e8f7142f6b82e28ad04
Ajinkya_Phadke <ajinkyaphadke7721@gmail.com>
2022-09-29 21:13:57 +0530

    assignment 29-09-22

---
+++ Ajinkya_Phadke/29-09-2022/for_loop.js
@@ -0,0 +1,23 @@
+let  n = 100; 
+while(n <=110){ 
+    console.log(n);
+    n = n + 1; 
+}
+
+let m = [20,30,40,50];
+for(let c = 1;c <= m.length; c= c +1){
+     console.log(c);
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+let f = "bbdjkknc,dfhgerhe,gfdgehet";
+while(f < f.length){
+
+}
+
+// =====================
+console.log("*");
+
+for(let j = 10; j >= 1; j = j -1){
+    console.log(j);
+}
