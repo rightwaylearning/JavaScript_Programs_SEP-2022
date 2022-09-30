b3e9f529cce270ce6bcd9b57e4236d36e46a6789
AshDcode <ashwinidewade23@gmail.com>
2022-09-30 12:25:42 +0530

    29/09 programs

---
+++ Nirmal_Rajput/functionprogram.js
@@ -0,0 +1,25 @@
+function friendsinfo(getinfo){
+
+    console.log(getinfo.firstfriend);
+    console.log(getinfo.secondfriend);
+    console.log(getinfo.thirdfriend);
+    console.log(getinfo.fourfriend);
+}
+
+friendsinfo({
+
+    firstfriend: "darpan",
+
+    secondfriend: "indra",
+
+    thirdfriend: "kuldip",
+
+    fourfriend: "prashant"
+
+})
+
+
+
+
+
+
