dead25eb472f512cebd7a1a21d80b2d83098de7d
dhanashri.udar <udardhanshri2018@gmail.com>
2022-10-03 16:46:48 +0530

    abc

---
+++ Dhanashri_Udar_Program/module.js/user.js
@@ -0,0 +1,13 @@
+let obj = module.require("./cal.js");
+
+let s = obj.add(10 , 20);
+console.log(s);
+
+let s1 = obj.substraction(10 , 20);
+console.log(s1);
+
+let d = obj.division(10 , 20);
+console.log(d);
+
+let m = obj.multiply(10 , 20);
+console.log(m);
