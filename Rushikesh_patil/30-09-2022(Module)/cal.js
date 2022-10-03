bfc48c74c5a0bc110cf87005e5b994f4d13b81ff
Rushikesh Patil <Rushikesh.patil@gmail.com>
2022-10-03 23:44:48 +0530

    30-09-2022

---
+++ Rushikesh_patil/30-09-2022(Module)/cal.js
@@ -0,0 +1,26 @@
+
+
+module.exports.sum=function sum(a,b){
+    let s = a + b;
+    console.log(s);
+}
+
+module.exports.sub=function sub(a,b){
+    let s1 = a - b;
+    console.log(s1);
+}
+
+module.exports.mul=function mul(a,b){
+    let m = a * b ;
+    console.log(m);
+}
+
+module.exports.div=function div(a,b){
+    let d = a / b ;
+    console.log(d);
+}
+
+
+
+
+
