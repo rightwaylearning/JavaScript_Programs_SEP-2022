4bf5dbc0fe03bb6cf3910e190d75628e18fce9c7
rutuja.nimbhore <rutujanimbhore1999@gmail.com>
2022-10-04 18:33:13 +0530

    new file

---
+++ Rutuja_Nimbhore_Program/ModuleProgram/cal.js
@@ -0,0 +1,16 @@
+ module.exports.add=function add(a,b)
+{
+    return a+b;
+}
+module.exports.sub=function sub(a,b)
+{
+    return a-b;
+}
+module.exports.mul=function mul(a,b)
+{
+    return a*b;
+}
+module.exports.div=function div(a,b)
+{
+    return a/b;
+}
