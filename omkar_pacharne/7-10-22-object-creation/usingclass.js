ae11035544313c3ae0f4a044757a99fdae39e91c
omkar pacharne <pacharneomkar06@gmail.com>
2022-10-07 15:02:01 +0530

    constructor program

---
+++ omkar_pacharne/7-10-22-object-creation/usingclass.js
@@ -0,0 +1,15 @@
+
+
+class student {
+    constructor(fName,lName){
+        this.firstName=fName;
+        this.lastName=lName;
+
+    }
+}
+let obj1=new student("omkar","shubham");
+console.log(obj1);
+let s=new student;
+console.log(s)
+let obj2=new student("rashmi","pratima");
+console.log(obj2);
