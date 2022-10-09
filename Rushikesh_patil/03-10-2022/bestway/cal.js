f1539a2fe1f9908420b05dc79dfcd8ebe6fe0975
Rushikesh Patil <Rushikesh.patil@gmail.com>
2022-10-09 12:36:52 +0530

    new commit

---
+++ Rushikesh_patil/03-10-2022/bestway/cal.js
@@ -0,0 +1,12 @@
+function sum(a,b){
+    s = a + b ;
+    console.log(s);
+}
+
+function sub(x,y){
+    s1 = x - y;
+    console.log(s1);
+}
+
+module.exports.add=sum;
+module.exports.minus=sub;
---
+++ Rushikesh_patil/03-10-2022/bestway/user.js
@@ -0,0 +1,11 @@
+let m1=module.require("./cal.js");
+let m2=module.require("./app.js");
+
+let v1=m1.add(10,40);
+let v2=m1.minus(30,90);
+let v3=m2.msg;
+
+console.log(v3);
+
+
+
