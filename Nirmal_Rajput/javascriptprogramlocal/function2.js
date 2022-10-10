3d590472bc2ab27feb99d849d790714cf389997e
Nirmal Rajput <nirmal12796@gmail.com>
2022-10-10 09:02:24 +0530

    new practice program

---
+++ Nirmal_Rajput/javascriptprogramlocal/function2.js
@@ -0,0 +1,46 @@
+       function takemarks(subject){
+        console.log(subject.subjectName);
+        console.log(subject.marks);
+}
+
+
+
+let marks = {
+subjectName: "marathi",
+outof : 50 ,
+marks : 45
+}
+
+takemarks(marks );
+
+
+
+                                                  function personalInfo(info){
+
+                                                                         console.log(info.myfirstName);
+                                                                         console.log(info.lastName);
+                                                                         console.log(info.mynum);
+                                                                 }
+
+
+
+
+
+                                                                      let myInfo = {
+
+                                                                                 myfirstName : "nirmal" ,
+                                                                                 lastName : "rajput" ,
+                                                                                 mynum : "8010649891"
+                                                                         }
+
+                                                 personalInfo(myInfo);
+
+
+
+
+
+
+
+
+
+
