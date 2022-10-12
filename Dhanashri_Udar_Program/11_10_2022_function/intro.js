38aa4e055fd90787bbfcb26a392778cf43a597b3
dhanashri.udar <udardhanshri2018@gmail.com>
2022-10-12 18:41:17 +0530

    fdfd

---
+++ Dhanashri_Udar_Program/11_10_2022_function/intro.js
@@ -0,0 +1,27 @@
+//1--
+ function test(){
+    console.log("I am plan function");
+ }
+ test();
+
+ //2
+
+ class A {
+    //method
+    test(){
+        //........
+
+    }
+ }
+ let o = new A();
+ o.test();
+
+ //3
+let obj = {
+    //method
+
+    key1:function(){
+
+    }
+}
+obj.key1();
