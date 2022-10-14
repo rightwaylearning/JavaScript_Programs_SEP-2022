6b8fbb9ff3b0c706e444b567951aee4752499542
vaibhav patil <vaibhavpatil8743@gmail.com>
2022-10-14 08:08:06 +0530

    19to23programs

---
+++ vaibhav_patil/19-09-2022/function_with_parameter.js
@@ -0,0 +1,11 @@
+//function decln
+
+function test(message){
+    let message1="Hey good morning";
+    test(message1);
+    console.log(message1);
+    
+
+    let message2="Hiii goood night";
+    test (message2);
+}
