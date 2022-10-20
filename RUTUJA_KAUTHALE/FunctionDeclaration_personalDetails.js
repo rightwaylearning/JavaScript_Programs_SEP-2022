3aa45fccc9fc0c50fb60467b1eb6f27c96dcb867
Rutuja <rutujakauthale28@gmail.com>
2022-10-20 08:23:23 +0530

    Rutuja JS function declaration program

---
+++ RUTUJA_KAUTHALE/FunctionDeclaration_personalDetails.js
@@ -0,0 +1,24 @@
+function showName(){
+let nickname="rutu";
+console.log("HI My name is Rutuja. but you can call me "+nickname);
+
+}
+function showAddress(){
+
+console.log("My address is AT POST Deodaithan. Tal-Shrigonda, Dist-Ahmednagar");
+
+}
+
+function myNumber()
+{
+
+let mobNumber= 9850615317;
+console.log("My mob number is "+mobNumber);
+}
+
+
+showName();
+showAddress();
+ myNumber();
+
+showName();
