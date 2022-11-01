9fa6483daed072c176b65a979f99aa7aba008944
Archana khaire <archanakhaire1986@gmail.com>
2022-11-01 15:06:59 -0400

    Arithmatic opertors program

---
+++ Archana _khaire/01-11-2022/Operators.js
@@ -0,0 +1,80 @@
+let a = 10;
+let b = 30;
+let sum = a+b;
+console.log(sum);
+
+
+let s = 25+30;
+console.log(s);//55
+
+//--------------//
+let s1= 'arc';
+let s2 = 'hana';
+let s3 = s1+s2;
+console.log(s3); // archana
+
+let sum1 = 'arc'+'hana';
+console.log(sum1);
+
+let sum2 = 20+'54'; // '2054'
+console.log(sum2);
+//-------------------
+
+let sum3 = 10+20+'30'; // '3030'
+console.log(sum3);
+
+let sum4 = '20'+10+25; // '201025'
+console.log(sum4); 
+//---------------------------
+
+let sum5 = 10+20+50+'10'; 
+           // 30+50+'10';
+           // 80+'10';
+           //'8010';
+
+console.log(sum5);
+
+//----------------
+
+let sum6 = '20'+20+10+25;
+           // '2020'+10+25;
+           //'20201025';
+console.log(sum6);
+
+
+let sum7 = 2+3+4+''+4+6;
+        // = 5+4+''+4+6;
+        // = 9+''+4+6;
+        // = '9'+ 4+6;
+        // = '946';
+console.log(sum7);
+
+
+// null = 0;
+// bollean   true=1  false = 0
+// undefined = NoN(Not a Number) 
+
+
+console.log(25+false); //25
+
+console.log(10+true); // 11
+
+console.log(20-null+true+null); //21
+
+console.log(true+true);
+ 
+console.log(25-true-null-true); //23
+
+console.log(true+undefined); //NaN
+
+//-----------------------
+
+console.log('258'+false); // '258false'
+
+console.log(true+true+false+''+true+null); // '2truenull'
+
+console.log(30+undefined); //NaN
+
+console.log('30'+undefined); //'30undefined'
+
+
