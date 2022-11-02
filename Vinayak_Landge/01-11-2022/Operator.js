8ca643189806a45510a91d0bb694bdcd46d91a5e
Vinayak Landge <vinay.landge@gmail.com>
2022-11-02 06:36:23 +0530

    operator

--- Vinayak_Landge/01-11-2022/Operator.js
+++ Vinayak_Landge/01-11-2022/Operator.js
@@ -15,3 +15,15 @@ console.log(12+50+'a');
 console.log(4+5+'y'+2+3);
 
 console.log(5+' '+6+5);
+
+console.log(3+true);
+
+console.log(true+true);
+
+console.log('r'+true);
+
+console.log(10-true+false-true);
+
+console.log(10+undefined);
+
+console.log('10'+undefined);
