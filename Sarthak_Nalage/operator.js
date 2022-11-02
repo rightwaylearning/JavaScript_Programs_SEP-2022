79508cf931d8ae752157735206c0a59b81e8242a
SARTHAK <sarthaknalage@gmail.com>
2022-11-02 09:48:59 +0530

    New

---
+++ Sarthak_Nalage/operator.js
@@ -0,0 +1,12 @@
+let s1='sarthak';
+let s2='Nalage';
+console.log(s1+s2);
+console.log(10+20+'23');
+let s3=('10'+220+23);
+console.log(typeof s3)
+let s4=1+2+3+' '+45;
+console.log(s4);  
+ console.log(null+true+false);
+ console.log(""+null+true+23+false);
+ s4=""+null+true+23+false;
+ console.log(typeof s4);
