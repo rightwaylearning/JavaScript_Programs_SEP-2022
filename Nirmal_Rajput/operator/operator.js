8354eeb9032e9191ee3a34534781fe2ee6a5eff3
Nirmal Rajput <nirmal12796@gmail.com>
2022-11-04 10:21:11 +0530

    operator

---
+++ Nirmal_Rajput/operator/operator.js
@@ -0,0 +1,43 @@
+
+
+
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c);
+
+console.log("---------------------------------");
+
+let a1 = 20+ 40;
+console.log(a1);
+
+console.log("---------------------------------");
+
+let b1 = 'a' + 10;
+console.log(b1);
+
+console.log("---------------------------------");
+
+let c1 = 'nir';
+let c2 = 'mal';
+let sum = c1 + c2;
+console.log(sum)
+
+console.log("---------------------------------");
+
+
+let z = 10 + '10';
+console.log(z);
+
+
+console.log("---------------------------------");
+
+let sum3 = 10 + 30 + 40 +'50';
+console.log(sum3)
+
+console.log("---------------------------------");
+
+console.log(10+true);
+console.log(10+true + false + null);
+console.log(true+true);
+console.log(1+undefined);
