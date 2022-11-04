c19cadefc312597c9d42759717b8711fa14397ed
Nirmal Rajput <nirmal12796@gmail.com>
2022-11-04 16:37:45 +0530

    assigment of operator

---
+++ Nirmal_Rajput/try_catch/try_catch.js
@@ -0,0 +1,17 @@
+
+function add(b,c){
+return b+c;
+}
+
+let sum;
+console.log("operatiom is here");
+
+try{
+    let sum = add1(10,20);
+} catch(e){
+    console.log(e.name);
+  console.log(e.name);
+}finally{
+
+    console.log("i am finally method");
+}
