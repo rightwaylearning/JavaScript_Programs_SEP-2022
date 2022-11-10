9aaf19b1c5b7c2ba9ca0fe60fdc429ba9bc71d37
sachin lagad <sachin.lagad@gmail.com>
2022-11-10 10:08:45 +0530

    data

---
+++ RightWayLearning/10-11-2022.-loops/for_loop.js
@@ -0,0 +1,16 @@
+
+let arr = [10,20,30,40,50,60,70,80,90,100];
+
+for(let index = 0; index < arr.length; index++){
+   if(arr[index] != 600){
+      if(index == arr.length - 1){
+        console.log("Number is not present");
+        break;
+      }
+      continue;
+   }
+
+   console.log(" 60 number index is "+ index);
+   break;
+
+}
