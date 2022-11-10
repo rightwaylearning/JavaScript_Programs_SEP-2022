8e54b932c5adb629edcb70ce003036890c0b952c
dhanashri.udar <udardhanshri2018@gmail.com>
2022-11-10 11:29:32 +0530

    dhdh

---
+++ Dhanashri_Udar_Program/8_11_22_flow_control/if_statement.js
@@ -0,0 +1,17 @@
+let age = 16;
+
+function exp1(){
+    return age;
+}
+// number zero >> false, non-zero true
+if(exp1() >= 18)
+    console.log("I can vote in election");
+
+
+console.log("end");
+
+// min
+
+if(exp1() > 18)
+console.log("try to avoid patries");
+console.log("it's ok");
