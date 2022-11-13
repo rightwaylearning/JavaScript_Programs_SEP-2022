b0b00daca3d063dcbaec06ec9ff2cffa6b661d47
Rutuja <rutujakauthale28@gmail.com>
2022-11-13 19:08:35 +0530

    flowcontrol

---
+++ RUTUJA_KAUTHALE/FlowControl/whileloop.js
@@ -0,0 +1,30 @@
+let P=0;
+console.log("Print 0 to 10 NUMBERS");
+while(P<=10)
+{
+    console.log(P);
+    P++;
+}
+P=0;
+
+//   Even number print
+console.log("Even NUMBERS");
+ while(P<=10){
+    if(P%2==0)
+    {
+        console.log(P);
+    }
+    P++;
+    
+ }
+ P=0;
+//   odd number print
+console.log("ODD NUMBERS");
+while(P<=10){
+    if(P%2==1)
+    {
+        console.log(P);
+    }
+    P++;
+    
+ }
